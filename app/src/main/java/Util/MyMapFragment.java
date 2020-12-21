c6cb23b1fba4adcf5df7b328a1dd1ddf493e8bcb
dzodd <haiduong6109@gmail.com>
2020-12-22 01:52:28 +0700

    chuc nang about va google map

---
+++ app/src/main/java/Util/MyMapFragment.java
@@ -0,0 +1,43 @@
+package Util;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MyMapFragment extends SupportMapFragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+
+    public MyMapFragment()  {
+        getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(final GoogleMap gmap) {
+        this.googleMap = gmap;
+
+        // Set default position
+        // Add a marker in Sydney and move the camera
+        LatLng vietnam = new LatLng(14.0583, 108.2772); // 14.0583° N, 108.2772° E
+        this.googleMap.addMarker(new MarkerOptions().position(vietnam).title("Marker in Vietnam"));
+        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(vietnam));
+
+        this.googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : "+ latLng.longitude);
+                // Clear previously click position.
+                googleMap.clear();
+                // Zoom the Marker
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                // Add Marker on Map
+                googleMap.addMarker(markerOptions);
+            }
+        });
+    }
+}
