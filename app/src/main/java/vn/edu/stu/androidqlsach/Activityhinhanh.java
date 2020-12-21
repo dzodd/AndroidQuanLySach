c9532654e9994c94336690e6f6824ae1868751e8
Dat <dtran0692@gmail.com>
2020-12-22 00:51:06 +0700

    chuc nang hien thi chi tiet sach va them sach

---
+++ app/src/main/java/vn/edu/stu/androidqlsach/Activityhinhanh.java
@@ -0,0 +1,14 @@
+package vn.edu.stu.androidqlsach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activityhinhanh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activityhinhanh);
+    }
+}
